7c646c5e0abc9a6cec2de28575937290525d2e58
RiderExMachina <riderexmachina@protonmail.com>
2023-10-03 02:00:09 -0700

    make the absolute basics work with the new format

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
-from bs4 import BeautifulSoup as soup
-import requests, time, argparse, logging, json
+import os, json
+import requests
+from mastodon import Mastodon
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--debug', help="Debug mode: will not post to Twitter or Mastodon", default=False, action='store_true')
-parser.add_argument('-a', '--add-project', help="Add project to watchlist", default=False, action='store_true')
-args = parser.parse_args()
+def relay(msg):
+    print(msg)
+authFile = "auth.cred"
+relay("\t - Looking for Mastodon information...")
+if os.path.isfile(authFile):
+    with open(authFile, "r") as Auth:
+        mast_info = json.load(Auth)["mastodon"]
+        relay("\tFound.")
+        for info in mast_info:
+            MAST_CONSUMER_KEY = info["mast_api_key"]
+            MAST_CONSUMER_SECRET = info["mast_api_secret"]
+            MAST_ACCESS_KEY = info["mast_access_key"]
+    relay("Mastodon information loaded successfully.")
+mastodonURL = "botsin.space"
+mastodon = Mastodon(client_id=MAST_CONSUMER_KEY, client_secret=MAST_CONSUMER_SECRET, access_token=MAST_ACCESS_KEY, api_base_url=mastodonURL)
+# End Mastodon remove
 
-version = "0.0.1"
-if __name__=="__main__":
-    if not os.isfile("settings.json"):
-        import setup
-        setup()
-    elif args.add_project:
-        import add_projects
-        add_project()
-    else:
-        print(f"Running script version {version}")
+def post(name, release, link, tags="#foss #OpenSource"):
+    message = f"{name} has updated to {release}!\nCheckout the update here: {link}\n{tags}"
+    relay(message)
+    mastodon.status_post(message)
 
+def get_github_info(url):
+    page = requests.get(url, timeout=120)
+    information = page.json()
 
+    latest = information[0]
+    directURL = latest['html_url']
+    release = latest['tag_name']
+
+    return directURL, release
+
+with open("settings.json", "r", encoding="utf-8") as settingsFile:
+    info = json.load(settingsFile)
+
+updated = False
+for item in info["projects"]:
+    project = info["projects"][item]
+    name = project["name"]
+    gh_link = project["url"]
+    api_link = project["api-url"]
+    cached_release = project["latest-release"]
+    tags = project["tags"]
+
+    update_link, updated_release = get_github_info(api_link)
+
+    if cached_release != updated_release:
+        post(name, updated_release, update_link, tags)
+        info["projects"][item]["url"] = update_link
+        info["projects"][item]["latest-release"] = updated_release
+        updated = True
+if updated:
+    with open("settings.json.new", "w", encoding="utf-8") as settingsFile:
+        json.dump(info, settingsFile, indent=4)
+    os.rename("settings.json.new", "settings.json")
+if not updated:
+    print("No updates found!")
